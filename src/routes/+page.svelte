a14ff28d7bdfd5fea056e27fdf664e49661b1c18
JadeZaher <contact@harrsoft.studio>
2025-01-27 19:29:07 -0700

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import logo from '$lib/assets/logo/image.png'
+</script>
+<div class="bg-slate-50 h-screen w-screen grid justify justify-center content-center">
+    <div class="m-10 flex justify-self-stretch flex-col max-w-4xl">
+        <div>
+            <img class="rounded-md my-5 shadow-md" alt="the logo" src={logo}>
+        </div>
+        <p class=" text-black text-lg">The Platform for Informed, Accountable, and Solution-Driven Civic Dialogue</p>
+        <button class="btn max-w-lg bg-black text-white mt-3">Learn More</button>
+    </div>
+</div>
