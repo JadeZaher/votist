63f25cb45daf4cce9da0486d7b9f0d2347ea217c
JadeZaher <contact@harrsoft.studio>
2025-02-08 14:19:35 -0700

    color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script>
     import logo from '$lib/assets/logo/VotistPreview.png'
 </script>
-<div class="bg-slate-50 h-screen w-screen grid justify justify-center content-center">
+<div class="bg-black h-screen w-screen grid justify justify-center content-center">
     <div class="m-10 flex justify-self-stretch flex-col max-w-6xl justify-center">
         <div>
             <img class="rounded-md my-5 shadow-md" alt="the logo" src={logo}>
         </div>
-        <p class=" text-black text-lg font-black">The Platform for Informed, Accountable, and Solution-Driven Civic Dialogue</p>
+        <p class=" text-white text-lg font-black">The Platform for Informed, Accountable, and Solution-Driven Civic Dialogue</p>
     </div>
 </div>
