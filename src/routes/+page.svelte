8c09761ed73c45419eb603d3622547793f36b8d1
JadeZaher <contact@harrsoft.studio>
2025-02-08 13:38:07 -0700

    image update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,11 @@
 <script>
-    import logo from '$lib/assets/logo/image.png'
+    import logo from '$lib/assets/logo/votistpreview.png'
 </script>
 <div class="bg-slate-50 h-screen w-screen grid justify justify-center content-center">
-    <div class="m-10 flex justify-self-stretch flex-col max-w-4xl">
+    <div class="m-10 flex justify-self-stretch flex-col max-w-6xl justify-center">
         <div>
             <img class="rounded-md my-5 shadow-md" alt="the logo" src={logo}>
         </div>
-        <p class=" text-black text-lg">The Platform for Informed, Accountable, and Solution-Driven Civic Dialogue</p>
-        <button class="btn max-w-lg bg-black text-white mt-3">Learn More</button>
+        <p class=" text-black text-lg font-black">The Platform for Informed, Accountable, and Solution-Driven Civic Dialogue</p>
     </div>
 </div>
