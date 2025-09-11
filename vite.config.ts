9452b04b48b6602a759f7109ae1e996dde9f656a
EliCarenza <elicarenza@proton.me>
2025-09-11 02:33:15 -0400

    Fixes docker issues for rancher desktop

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 
+	server: {
+		host: '0.0.0.0',
+		port: 5173,
+		watch: {
+			usePolling: true,
+			interval: 500, // Check for changes every 0.5 seconds
+			ignored: ['**/.git/**']
+		}
+	},
+
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
